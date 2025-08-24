e68b02ad89815c76f907728b9d06c025e3252e15
Lars Smith Eier <larssmitheier@gmail.com>
2025-08-24 17:36:27 +0200

    switched locale from en_US to en_GB, and deterministically enabled NTP

--- modules/core/locale.nix
+++ modules/core/locale.nix
@@ -20,22 +20,36 @@ in
     # timezone
     time.timeZone = "Europe/Oslo";
 
+    # NTP
+    services.timesyncd = {
+      enable = true;
+      servers = [
+        "0.no.pool.ntp.org"
+        "1.no.pool.ntp.org"
+        "2.no.pool.ntp.org"
+        "3.no.pool.ntp.org"
+      ];
+    };
+
     # locale
-    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.defaultLocale = "en_GB.UTF-8";
     i18n.extraLocaleSettings = {
-      LC_ADDRESS = "nb_NO.UTF-8"; # regional address format
-      LC_IDENTIFICATION = "nb_NO.UTF-8";
-      LC_MEASUREMENT = "nb_NO.UTF-8"; # metric system (kg, °C)
-      LC_MONETARY = "nb_NO.UTF-8"; # NOK currency
-      LC_NAME = "nb_NO.UTF-8";
-      LC_PAPER = "nb_NO.UTF-8"; # A4
+      # English for UI text and tool behavior
+      LC_MESSAGES = "en_GB.UTF-8";
+      LC_RESPONSE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_COLLATE = "en_GB.UTF-8";
+      LC_CTYPE = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+
+      # Norwegian for administrative and formatting categories
+      LC_MONETARY = "nb_NO.UTF-8";
+      LC_MEASUREMENT = "nb_NO.UTF-8";
+      LC_PAPER = "nb_NO.UTF-8";
+      LC_ADDRESS = "nb_NO.UTF-8";
       LC_TELEPHONE = "nb_NO.UTF-8";
-      #
-      LC_MESSAGES = "en_US.UTF-8"; # CLI output, manpages (no "colour")
-      LC_CTYPE = "en_US.UTF-8"; # must match LANG
-      LC_COLLATE = "en_US.UTF-8"; # sort order
-      LC_NUMERIC = "en_US.UTF-8"; # decimal point (`.`), thousands sep
-      LC_TIME = "en_GB.UTF-8"; # 24h time, ISO dates, still English month names
+      LC_IDENTIFICATION = "nb_NO.UTF-8";
     };
 
     # xserver defaults
