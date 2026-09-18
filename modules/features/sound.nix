d93df0b05ecfe903c798ac6866e5fdd511970fb0
Lars Smith Eier <larssmitheier@protonmail.com>
2026-09-18 18:42:21 +0200

    chore(sound): drop pipewire options that match upstream defaults
    alsa.support32Bit was the only non-default setting left in this block;
    removing it falls back to upstream's false and drops the 32-bit ALSA
    plumbing. jack.enable was already false by default.
    
    Signed-off-by: Lars Smith Eier <larssmitheier@protonmail.com>

--- modules/features/sound.nix
+++ modules/features/sound.nix
@@ -3,9 +3,7 @@
     services.pipewire = {
       enable = true;
       alsa.enable = true;
-      alsa.support32Bit = true;
       pulse.enable = true;
-      jack.enable = false;
     };
 
     security.rtkit.enable = true;
